5395bbf9ab050f598428487510002b5b952e377d
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-06 10:37:35 -0300

    aula 151: Java operacoes

---
+++ JavaScript/operacoes.js
@@ -0,0 +1,15 @@
+/*Precedência de execução*/
+/*
+0) Parêntese
+1)multiplicação de divisão
+2) Adição e subtração
+*/
+
+//4 + 4 /2
+//2 + 2 / 2 *3 // 2 + 1 * 3
+//2 + 2 / 2 *3 // 2 + 4 * 3
+// 2 * 2 / 2
+//5 - 5 + 2
+
+let res = (4 + 4) / 2
+console.log(res)
