9d3aade117a88b19228d4c3dda5f85441d782764
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-06 11:46:38 -0300

    Aula 152: JS Operadores relacionais

---
+++ JavaScript/operadores-relacionais.js
@@ -0,0 +1,45 @@
+/*
+Operadores relacionais (retorna verdadeiro ou falso)
+== (igua a)
+!= (diferente)
+> (maior que)
+< (menor que)
+>= (maior ou igual)
+<= (menor ou igual)
+*/
+
+
+// let n1 = 20
+// let n2 = 10
+//let idade = 25
+
+
+// let pedidoMinimo = 20
+// let compraPessoa = 100
+// let resultado = compraPessoa >= pedidoMinimo
+// console.log(resultado);
+
+
+/* Operadores lógicos (testa condições com true ou false)
+&& (e)
+|| (ou)
+*/
+
+// let resultado = true && true
+// console.log(resultado);
+
+// let resultado = 2 == 2 && 1 == 3
+// console.log(resultado);
+
+// let resultado = 2 == 1 || 1 == 3
+// console.log(resultado);
+
+// let idade = 15
+
+// let resultado = idade >= 18 && idade <= 45
+// console.log(resultado);
+
+let idade = 25
+let totalCompras = 200
+let resultado = idade >= 40 || totalCompras >= 200
+console.log(resultado);
