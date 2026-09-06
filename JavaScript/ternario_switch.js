c87891cfad7a48f06b6513499fda9acfa7c7051c
Pedro Henrique Moreci Manhães de Oliveira <pedrohmanhaes@gmail.com>
2026-09-06 16:00:12 -0300

    Aula 154: JS ternario_switch

---
+++ JavaScript/ternario_switch.js
@@ -0,0 +1,37 @@
+/*Operador ternário é uma forma de simplificar 
+o if e else, ele é representado pelo símbolo "?" e ":" */
+
+/*switch case é uma forma de simplificar o if e else, 
+ele é representado pelo símbolo "switch" e "case" 
+3
+let opcao = 1
+
+if(opcao == 1) {
+    console.log("Saldo conta"); 
+} else if(opcao == 2) {
+    console.log("Extrato conta");
+} else if(opcao == 3) {
+    console.log("cartão de crédito");
+} else {
+    console.log("Opção inválida");
+}
+    */
+
+// let opcao = 4
+
+// switch(opcao) {
+//     case 1:
+//         console.log("Saldo conta");
+//         break;
+//     case 2:
+//         console.log("Extrato conta");
+//         break;
+//     case 3:
+//         console.log("cartão de crédito");
+//         break;
+//     default:
+//         console.log("Opção inválida");
+// }
+
+let idade = 15
+console.log(idade >= 18 ? "Maior idade" : "Menor idade")
